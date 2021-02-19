18ba0c7fdcbd9c6bfed8de8f12221f2b7aaaa86e
Coleman-Wimpy <coleman.wimpy@protonmail.com>
2021-02-19 17:36:01 -0500

    Update dependencies to Jakarta instead of Java because we are using Tomcat 10.x. Servlets now work.

--- src/main/java/com/example/XamineRIS_RE_JAVA/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.XamineRIS_RE_JAVA;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
---
+++ src/main/java/loginServlet.java
@@ -0,0 +1,22 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.*;
+
+@WebServlet(name = "loginServlet", value = "/login")
+public class loginServlet extends HttpServlet {
+
+    public void init() {}
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void destroy(){
+
+    }
+}
